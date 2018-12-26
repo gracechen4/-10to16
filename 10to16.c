c5e1b7dab9a091d9becef6f63594b67c19f9cc26
AdityaAtulTewari <adityaatewari@gmail.com>
2018-12-26 14:01:17 -0600

    Added preliminary program stub

---
+++ 10to16.c
@@ -0,0 +1,16 @@
+/**************************************************************************************************************************************************************
+ * The goal of this program is to take a number in base 10 (as a string, of size less than uint64_t or equal to)
+ * negative number are allowed as in the first character could be a '-', and you need to deal with that to print negative numbers.
+ * The string "-", is valid and in our scheme is just the number 0.
+ * The only two libraries allowed are stdio and stdint
+ * argv[1] = input
+ *************************************************************************************************************************************************************/
+
+int main(int argc, char* argv[])
+{
+  //Ensure the input is exists.
+  //Ensure the input is valid.
+  //Parse the input
+  //Print the output
+  //Return -1 if an error occured, printing the error.
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# How do strings work in C? (Or really everything)
+
+Every string is a character array.
+
+So let's say I have a string like ```""```.
+|0|
+|"\0"|
