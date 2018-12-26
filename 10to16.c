30f029798a02221dbcc85a86057cca3efbce93ee
AdityaAtulTewari <adityaatewari@gmail.com>
2018-12-26 15:45:27 -0600

    All donso

--- 10to16.c
+++ 10to16.c
@@ -2,8 +2,12 @@
  * The goal of this program is to take a number in base 10 (as a string, of size less than uint64_t or equal to)
  * negative number are allowed as in the first character could be a '-', and you need to deal with that to print negative numbers.
  * The string "-", is valid and in our scheme is just the number 0.
- * The only two libraries allowed are stdio and stdint
+ * The only three libraries allowed are stdio, stdint, stdlib
  * argv[1] = input
+ *
+ * After the assignment come up with two alternative schemes for packing strings and discuss in 1000 words the strengths and weaknesses of your approaches, vs
+ * the current approach.
+ * Be sure to cleanup the memory you use. (in other words leak checking is in the grading system.)
  *************************************************************************************************************************************************************/
 
 int main(int argc, char* argv[])
